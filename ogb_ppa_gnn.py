3952fc37c5f808b88091a3d73529b5938abbec0b
muhanzhang <muhan.zhang@hotmail.com>
2020-05-22 01:29:49 -0700

    add ppa exp

---
+++ ogb_ppa_gnn.py
@@ -0,0 +1,408 @@
+import torch
+from torch_geometric.nn import MessagePassing
+from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool, GlobalAttention, Set2Set
+import torch.nn.functional as F
+from torch_geometric.nn.inits import uniform
+from torch_geometric.utils import degree
+import math
+
+from torch_scatter import scatter_mean
+
+class GNN(torch.nn.Module):
+
+    def __init__(self, num_class, num_layer = 5, emb_dim = 300, 
+                    gnn_type = 'gin', virtual_node = True, residual = False, drop_ratio = 0.5, JK = "last", graph_pooling = "mean", 
+                    use_hop_label=False, h=None, **kwargs):
+        '''
+            num_tasks (int): number of labels to be predicted
+            virtual_node (bool): whether to add virtual node or not
+        '''
+
+        super(GNN, self).__init__()
+
+        self.num_layer = num_layer
+        self.drop_ratio = drop_ratio
+        self.JK = JK
+        self.emb_dim = emb_dim
+        self.num_class = num_class
+        self.graph_pooling = graph_pooling
+        self.subgraph_pooling = subgraph_pooling
+
+        if self.num_layer < 2:
+            raise ValueError("Number of GNN layers must be greater than 1.")
+
+        ### GNN to generate node embeddings
+        if virtual_node:
+            self.gnn_node = GNN_node_Virtualnode(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type, use_hop_label=use_hop_label, h=h)
+        else:
+            self.gnn_node = GNN_node(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type, use_hop_label=use_hop_label, h=h)
+
+
+        ### Pooling function to generate whole-graph embeddings
+        if self.graph_pooling == "sum":
+            self.pool = global_add_pool
+        elif self.graph_pooling == "mean":
+            self.pool = global_mean_pool
+        elif self.graph_pooling == "max":
+            self.pool = global_max_pool
+        elif self.graph_pooling == "attention":
+            self.pool = GlobalAttention(gate_nn = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, 1)))
+        elif self.graph_pooling == "set2set":
+            self.pool = Set2Set(emb_dim, processing_steps = 2)
+        else:
+            raise ValueError("Invalid graph pooling type.")
+
+        if graph_pooling == "set2set":
+            self.graph_pred_linear = torch.nn.Linear(2*self.emb_dim, self.num_class)
+        else:
+            self.graph_pred_linear = torch.nn.Linear(self.emb_dim, self.num_class)
+
+        ### Pooling function to generate sub-graph embeddings
+        if self.subgraph_pooling == "sum":
+            self.subpool = global_add_pool
+        elif self.subgraph_pooling == "mean":
+            self.subpool = global_mean_pool
+        elif self.subgraph_pooling == "max":
+            self.subpool = global_max_pool
+        elif self.subgraph_pooling == "attention":
+            self.subpool = GlobalAttention(gate_nn = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, 1)))
+        else:
+            self.subpool = None
+
+    def forward(self, data):
+        x = self.gnn_node(data)
+
+        if 'node_to_subgraph' in data and 'subgraph_to_graph' in data:
+            x = self.subpool(x, data.node_to_subgraph)
+            x = self.pool(x, data.subgraph_to_graph)
+        else:
+            x = self.pool(x, data.batch)
+
+        return self.graph_pred_linear(x)
+
+
+class NestedGNN(torch.nn.Module):
+    # support multiple h
+
+    def __init__(self, num_class, num_layer = 5, emb_dim = 300, 
+                    gnn_type = 'gin', virtual_node = True, residual = False, drop_ratio = 0.5, JK = "last", graph_pooling = "mean", subgraph_pooling = "mean", 
+                    use_hop_label=False, hs=None, **kwargs):
+        '''
+            num_tasks (int): number of labels to be predicted
+            virtual_node (bool): whether to add virtual node or not
+        '''
+
+        super(NestedGNN, self).__init__()
+
+        self.num_layer = num_layer  # actual num_layer = num_layer + h for each h
+        self.drop_ratio = drop_ratio
+        self.JK = JK
+        self.emb_dim = emb_dim
+        self.num_class = num_class
+        self.graph_pooling = graph_pooling
+        self.subgraph_pooling = subgraph_pooling
+        self.hs = hs
+
+        if self.num_layer < 2:
+            raise ValueError("Number of GNN layers must be greater than 1.")
+
+        ### GNN to generate node embeddings
+        self.gnn_node = torch.nn.ModuleList()
+        for h in hs:
+            if virtual_node:
+                self.gnn_node.append(GNN_node_Virtualnode(h + num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type, use_hop_label=use_hop_label, h=h))
+            else:
+                self.gnn_node.append(GNN_node(h + num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type, use_hop_label=use_hop_label, h=h))
+
+        ### Pooling function to generate whole-graph embeddings
+        if self.graph_pooling == "sum":
+            self.pool = global_add_pool
+        elif self.graph_pooling == "mean":
+            self.pool = global_mean_pool
+        elif self.graph_pooling == "max":
+            self.pool = global_max_pool
+        elif self.graph_pooling == "attention":
+            self.pool = GlobalAttention(gate_nn = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, 1)))
+        elif self.graph_pooling == "set2set":
+            self.pool = Set2Set(emb_dim, processing_steps = 2)
+        else:
+            raise ValueError("Invalid graph pooling type.")
+
+        #self.fc = torch.nn.Linear(self.emb_dim * len(hs), self.emb_dim)
+        if graph_pooling == "set2set":
+            self.graph_pred_linear = torch.nn.Linear(2*self.emb_dim*len(hs), self.num_class)
+        else:
+            self.graph_pred_linear = torch.nn.Linear(self.emb_dim*len(hs), self.num_class)
+        
+
+        ### Pooling function to generate sub-graph embeddings
+        if self.subgraph_pooling == "sum":
+            self.subpool = global_add_pool
+        elif self.subgraph_pooling == "mean":
+            self.subpool = global_mean_pool
+        elif self.subgraph_pooling == "max":
+            self.subpool = global_max_pool
+        elif self.subgraph_pooling == "attention":
+            self.subpool = GlobalAttention(gate_nn = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, 1)))
+        else:
+            self.subpool = None
+
+
+    def forward(self, data_multi_hop):
+        x_multi_hop = []
+        for i, h in enumerate(self.hs):
+            data = data_multi_hop[h]
+            x = self.gnn_node[i](data)
+
+            if 'node_to_subgraph' in data and 'subgraph_to_graph' in data:
+                x = self.subpool(x, data.node_to_subgraph)
+                x = self.pool(x, data.subgraph_to_graph)
+            else:
+                x = self.pool(x, data.batch)
+            
+            x_multi_hop.append(x)
+
+        x = torch.cat(x_multi_hop, 1)
+        #x = F.elu(self.fc(x))
+
+        return self.graph_pred_linear(x)
+
+
+
+### GIN convolution along the graph structure
+class GINConv(MessagePassing):
+    def __init__(self, emb_dim):
+        '''
+            emb_dim (int): node embedding dimensionality
+        '''
+
+        super(GINConv, self).__init__(aggr = "add")
+
+        self.mlp = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, emb_dim))
+        self.eps = torch.nn.Parameter(torch.Tensor([0]))
+        
+        self.edge_encoder = torch.nn.Linear(7, emb_dim)
+
+    def forward(self, x, edge_index, edge_attr):
+        edge_embedding = self.edge_encoder(edge_attr)    
+        out = self.mlp((1 + self.eps) *x + self.propagate(edge_index, x=x, edge_attr=edge_embedding))
+    
+        return out
+
+    def message(self, x_j, edge_attr):
+        return F.relu(x_j + edge_attr)
+        
+    def update(self, aggr_out):
+        return aggr_out
+
+### GCN convolution along the graph structure
+class GCNConv(MessagePassing):
+    def __init__(self, emb_dim):
+        super(GCNConv, self).__init__(aggr='add')
+
+        self.linear = torch.nn.Linear(emb_dim, emb_dim)
+        self.root_emb = torch.nn.Embedding(1, emb_dim)
+        self.edge_encoder = torch.nn.Linear(7, emb_dim)
+
+    def forward(self, x, edge_index, edge_attr):
+        x = self.linear(x)
+        edge_embedding = self.edge_encoder(edge_attr)
+
+        row, col = edge_index
+
+        #edge_weight = torch.ones((edge_index.size(1), ), device=edge_index.device)
+        deg = degree(row, x.size(0), dtype = x.dtype) + 1
+        deg_inv_sqrt = deg.pow(-0.5)
+        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+
+        norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
+
+        return self.propagate(edge_index, x=x, edge_attr = edge_embedding, norm=norm) + F.relu(x + self.root_emb.weight) * 1./deg.view(-1,1)
+
+    def message(self, x_j, edge_attr, norm):
+        return norm.view(-1, 1) * F.relu(x_j + edge_attr)
+
+    def update(self, aggr_out):
+        return aggr_out
+
+
+### GNN to generate node embedding
+class GNN_node(torch.nn.Module):
+    """
+    Output:
+        node representations
+    """
+    def __init__(self, num_layer, emb_dim, drop_ratio = 0.5, JK = "last", residual = False, gnn_type = 'gin', use_hop_label=False, h=None):
+        '''
+            emb_dim (int): node embedding dimensionality
+            num_layer (int): number of GNN message passing layers
+
+        '''
+
+        super(GNN_node, self).__init__()
+        self.num_layer = num_layer
+        self.drop_ratio = drop_ratio
+        self.JK = JK
+        ### add residual connection or not
+        self.residual = residual
+
+        if self.num_layer < 2:
+            raise ValueError("Number of GNN layers must be greater than 1.")
+
+        if not use_hop_label:
+            self.node_encoder = torch.nn.Embedding(1, emb_dim) # uniform input node embedding
+        else:
+            self.node_encoder = torch.nn.Embedding(h+1, emb_dim)
+
+        ###List of GNNs
+        self.convs = torch.nn.ModuleList()
+        self.batch_norms = torch.nn.ModuleList()
+
+        for layer in range(num_layer):
+            if gnn_type == 'gin':
+                self.convs.append(GINConv(emb_dim))
+            elif gnn_type == 'gcn':
+                self.convs.append(GCNConv(emb_dim))
+            else:
+                ValueError('Undefined GNN type called {}'.format(gnn_type))
+                
+            self.batch_norms.append(torch.nn.BatchNorm1d(emb_dim))
+
+    def forward(self, batched_data):
+        x, edge_index, edge_attr, batch = batched_data.x, batched_data.edge_index, batched_data.edge_attr, batched_data.batch
+
+
+        ### computing input node embedding
+
+        h_list = [self.node_encoder(x)]
+        for layer in range(self.num_layer):
+
+            h = self.convs[layer](h_list[layer], edge_index, edge_attr)
+            h = self.batch_norms[layer](h)
+
+            if layer == self.num_layer - 1:
+                #remove relu for the last layer
+                h = F.dropout(h, self.drop_ratio, training = self.training)
+            else:
+                h = F.dropout(F.relu(h), self.drop_ratio, training = self.training)
+
+            if self.residual:
+                h += h_list[layer]
+
+            h_list.append(h)
+
+        ### Different implementations of Jk-concat
+        if self.JK == "last":
+            node_representation = h_list[-1]
+        elif self.JK == "sum":
+            node_representation = 0
+            for layer in range(self.num_layer):
+                node_representation += h_list[layer]
+
+        return node_representation
+
+
+### Virtual GNN to generate node embedding
+class GNN_node_Virtualnode(torch.nn.Module):
+    """
+    Output:
+        node representations
+    """
+    def __init__(self, num_layer, emb_dim, drop_ratio = 0.5, JK = "last", residual = False, gnn_type = 'gin', use_hop_label=False, h=None):
+        '''
+            emb_dim (int): node embedding dimensionality
+        '''
+
+        super(GNN_node_Virtualnode, self).__init__()
+        self.num_layer = num_layer
+        self.drop_ratio = drop_ratio
+        self.JK = JK
+        ### add residual connection or not
+        self.residual = residual
+
+        if self.num_layer < 2:
+            raise ValueError("Number of GNN layers must be greater than 1.")
+
+        if not use_hop_label:
+            self.node_encoder = torch.nn.Embedding(1, emb_dim) # uniform input node embedding
+        else:
+            self.node_encoder = torch.nn.Embedding(h+1, emb_dim)
+
+        ### set the initial virtual node embedding to 0.
+        self.virtualnode_embedding = torch.nn.Embedding(1, emb_dim)
+        torch.nn.init.constant_(self.virtualnode_embedding.weight.data, 0)
+
+        ### List of GNNs
+        self.convs = torch.nn.ModuleList()
+        ### batch norms applied to node embeddings
+        self.batch_norms = torch.nn.ModuleList()
+
+        ### List of MLPs to transform virtual node at every layer
+        self.mlp_virtualnode_list = torch.nn.ModuleList()
+
+        for layer in range(num_layer):
+            if gnn_type == 'gin':
+                self.convs.append(GINConv(emb_dim))
+            elif gnn_type == 'gcn':
+                self.convs.append(GCNConv(emb_dim))
+            else:
+                ValueError('Undefined GNN type called {}'.format(gnn_type))
+
+            self.batch_norms.append(torch.nn.BatchNorm1d(emb_dim))
+
+        for layer in range(num_layer - 1):
+            self.mlp_virtualnode_list.append(torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), \
+                                                    torch.nn.Linear(2*emb_dim, emb_dim), torch.nn.BatchNorm1d(emb_dim), torch.nn.ReLU()))
+
+
+    def forward(self, batched_data):
+
+        x, edge_index, edge_attr, batch = batched_data.x, batched_data.edge_index, batched_data.edge_attr, batched_data.batch
+
+        ### virtual node embeddings for graphs
+        virtualnode_embedding = self.virtualnode_embedding(torch.zeros(batch[-1].item() + 1).to(edge_index.dtype).to(edge_index.device))
+
+        h_list = [self.node_encoder(x)]
+        for layer in range(self.num_layer):
+            ### add message from virtual nodes to graph nodes
+            h_list[layer] = h_list[layer] + virtualnode_embedding[batch]
+
+            ### Message passing among graph nodes
+            h = self.convs[layer](h_list[layer], edge_index, edge_attr)
+
+            h = self.batch_norms[layer](h)
+            if layer == self.num_layer - 1:
+                #remove relu for the last layer
+                h = F.dropout(h, self.drop_ratio, training = self.training)
+            else:
+                h = F.dropout(F.relu(h), self.drop_ratio, training = self.training)
+
+            if self.residual:
+                h = h + h_list[layer]
+
+            h_list.append(h)
+
+            ### update the virtual nodes
+            if layer < self.num_layer - 1:
+                ### add message from graph nodes to virtual nodes
+                virtualnode_embedding_temp = global_add_pool(h_list[layer], batch) + virtualnode_embedding
+                ### transform virtual nodes using MLP
+
+                if self.residual:
+                    virtualnode_embedding = virtualnode_embedding + F.dropout(self.mlp_virtualnode_list[layer](virtualnode_embedding_temp), self.drop_ratio, training = self.training)
+                else:
+                    virtualnode_embedding = F.dropout(self.mlp_virtualnode_list[layer](virtualnode_embedding_temp), self.drop_ratio, training = self.training)
+
+        ### Different implementations of Jk-concat
+        if self.JK == "last":
+            node_representation = h_list[-1]
+        elif self.JK == "sum":
+            node_representation = 0
+            for layer in range(self.num_layer):
+                node_representation += h_list[layer]
+        
+        return node_representation
+
+
+if __name__ == '__main__':
+    GNN(num_class = 10)
--- run_ogb_ppa.py
+++ run_ogb_ppa.py
@@ -17,10 +17,9 @@ from dataset_pyg import PygGraphPropPredDataset  # customized to support pre_tra
 from dataloader import DataLoader  # use a custom dataloader to handle subgraphs
 from utils import create_subgraphs
 
-cls_criterion = torch.nn.BCEWithLogitsLoss()
-reg_criterion = torch.nn.MSELoss()
+multicls_criterion = torch.nn.CrossEntropyLoss()
 
-def train(model, device, loader, optimizer, task_type):
+def train(model, device, loader, optimizer):
     model.train()
 
     for step, batch in enumerate(tqdm(loader, desc="Iteration")):
@@ -36,16 +35,14 @@ def train(model, device, loader, optimizer, task_type):
         else:
             pred = model(batch)
             optimizer.zero_grad()
-            ## ignore nan targets (unlabeled) when computing training loss.
+
             if args.multiple_h is not None:
                 y = batch[args.h[0]].y
             else:
                 y = batch.y
-            is_labeled = y == y
-            if "classification" in task_type: 
-                loss = cls_criterion(pred.to(torch.float32)[is_labeled], y.to(torch.float32)[is_labeled])
-            else:
-                loss = reg_criterion(pred.to(torch.float32)[is_labeled], y.to(torch.float32)[is_labeled])
+
+            loss = multicls_criterion(pred.to(torch.float32), y.view(-1,))
+            
             loss.backward()
             optimizer.step()
 
@@ -72,8 +69,8 @@ def eval(model, device, loader, evaluator):
                 y = batch[args.h[0]].y
             else:
                 y = batch.y
-            y_true.append(y.view(pred.shape).detach().cpu())
-            y_pred.append(pred.detach().cpu())
+            y_true.append(y.view(-1,1).detach().cpu())
+            y_pred.append(torch.argmax(pred.detach(), dim = 1).view(-1,1).cpu())
 
     y_true = torch.cat(y_true, dim = 0).numpy()
     y_pred = torch.cat(y_pred, dim = 0).numpy()
@@ -83,8 +80,12 @@ def eval(model, device, loader, evaluator):
     return evaluator.eval(input_dict)
 
 
+def add_zeros(data):
+    data.x = torch.zeros(data.num_nodes, dtype=torch.long)
+    return data
+
 # Training settings
-parser = argparse.ArgumentParser(description='GNN baselines on ogbgmol* data with Pytorch Geometrics')
+parser = argparse.ArgumentParser(description='GNN baselines on ogbg-ppa data with Pytorch Geometrics')
 parser.add_argument('--gnn', type=str, default='gin-virtual',
                     help='GNN gin, gin-virtual, or gcn, or gcn-virtual (default: gin-virtual)')
 parser.add_argument('--subgraph', action='store_true', default=False, 
@@ -108,10 +109,8 @@ parser.add_argument('--epochs', type=int, default=100,
                     help='number of epochs to train (default: 100)')
 parser.add_argument('--num_workers', type=int, default=0,
                     help='number of workers (default: 0)')
-parser.add_argument('--dataset', type=str, default="ogbg-molhiv",
-                    help='dataset name (ogbg-molhiv, ogbg-molpcba, etc.)')
-parser.add_argument('--feature', type=str, default="full",
-                    help='full feature or simple feature')
+parser.add_argument('--dataset', type=str, default="ogbg-ppa",
+                    help='dataset name (default: ogbg-ppa)')
 parser.add_argument('--scheduler', action='store_true', default=False, 
                     help='use a scheduler to reduce learning rate')
 parser.add_argument('--seed', type=int, default=1)
@@ -128,14 +127,16 @@ np.random.seed(args.seed)
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 if args.multiple_h is not None:
-    from ogb_mol_gnn import NestedGNN as GNN
+    from ogb_ppa_gnn import NestedGNN as GNN
     args.h = [int(h) for h in args.multiple_h.split(',')]
 else:
-    from ogb_mol_gnn import GNN
+    from ogb_ppa_gnn import GNN
 
 path = 'data/ogb'
 pre_transform = None
+transform = add_zeros
 if args.subgraph:
+    transform = None
     if type(args.h) == int:
         path += '/sg_' + str(args.h)
     elif type(args.h) == list:
@@ -145,7 +146,7 @@ if args.subgraph:
     pre_transform = lambda x: create_subgraphs(x, args.h, args.use_hop_label, one_hot=False)
 ### automatic dataloading and splitting
 dataset = PygGraphPropPredDataset(
-    name=args.dataset, root=path, pre_transform=pre_transform, 
+    name=args.dataset, root=path, transform=transform, pre_transform=pre_transform, 
     skip_collate=args.multiple_h is not None)
 
 if False:  # visualize some graphs
@@ -178,14 +179,6 @@ if False:  # visualize some graphs
         f.savefig('tmp_vis.png')
         pdb.set_trace()
 
-if args.feature == 'full':
-    pass 
-elif args.feature == 'simple':
-    print('using simple feature')
-    # only retain the top two node/edge features
-    two_or_three = 3 if args.use_hop_label else 2
-    dataset.data.x = dataset.data.x[:,:two_or_three]
-    dataset.data.edge_attr = dataset.data.edge_attr[:,:2]
 
 split_idx = dataset.get_idx_split()
 
@@ -203,13 +196,13 @@ kwargs = {
 }
 
 if args.gnn == 'gin':
-    model = GNN(gnn_type = 'gin', num_tasks = dataset.num_tasks, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False, **kwargs).to(device)
+    model = GNN(gnn_type = 'gin', num_class = dataset.num_classes, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False, **kwargs).to(device)
 elif args.gnn == 'gin-virtual':
-    model = GNN(gnn_type = 'gin', num_tasks = dataset.num_tasks, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True, **kwargs).to(device)
+    model = GNN(gnn_type = 'gin', num_class = dataset.num_classes, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True, **kwargs).to(device)
 elif args.gnn == 'gcn':
-    model = GNN(gnn_type = 'gcn', num_tasks = dataset.num_tasks, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False, **kwargs).to(device)
+    model = GNN(gnn_type = 'gcn', num_class = dataset.num_classes, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False, **kwargs).to(device)
 elif args.gnn == 'gcn-virtual':
-    model = GNN(gnn_type = 'gcn', num_tasks = dataset.num_tasks, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True, **kwargs).to(device)
+    model = GNN(gnn_type = 'gcn', num_class = dataset.num_classes, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True, **kwargs).to(device)
 else:
     raise ValueError('Invalid GNN type')
 
@@ -227,28 +220,24 @@ train_curve = []
 for epoch in range(1, args.epochs + 1):
     print("=====Epoch {}".format(epoch))
     print('Training...')
-    train(model, device, train_loader, optimizer, dataset.task_type)
+    train(model, device, train_loader, optimizer)
 
     print('Evaluating...')
     train_perf = eval(model, device, train_loader, evaluator)
     valid_perf = eval(model, device, valid_loader, evaluator)
     test_perf = eval(model, device, test_loader, evaluator)
-    
+
     if args.scheduler:
         scheduler.step()
 
     print({'Train': train_perf, 'Validation': valid_perf, 'Test': test_perf})
 
-    train_curve.append(train_perf[dataset.eval_metric])
-    valid_curve.append(valid_perf[dataset.eval_metric])
-    test_curve.append(test_perf[dataset.eval_metric])
+    train_curve.append(train_perf['acc'])
+    valid_curve.append(valid_perf['acc'])
+    test_curve.append(test_perf['acc'])
 
-if 'classification' in dataset.task_type:
-    best_val_epoch = np.argmax(np.array(valid_curve))
-    best_train = max(train_curve)
-else:
-    best_val_epoch = np.argmin(np.array(valid_curve))
-    best_train = min(train_curve)
+best_val_epoch = np.argmax(np.array(valid_curve))
+best_train = max(train_curve)
 
 print('Finished training!')
 print('Best validation score: {}'.format(valid_curve[best_val_epoch]))
