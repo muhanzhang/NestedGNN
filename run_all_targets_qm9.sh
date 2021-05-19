3826075ec41c97a19416dbb91ffcfb05eb9af8a9
muhanzhang <muhan.zhang@hotmail.com>
2021-05-19 17:05:08 +0800

    update run_all_qm9

--- run_all_targets_qm9.sh
+++ run_all_targets_qm9.sh
@@ -9,18 +9,17 @@ T0=${1}
 T1=${2}
 for target in $(seq ${T0} ${T1})
 do
-  '''
-  python run_qm9.py --h 3 --model k1_GNN_sub --save_appendix _k1_h3_spd_rd --use_rd --target ${target}
-  python run_qm9.py --h 3 --model k12_GNN_sub --save_appendix _k12_h3_spd_rd --use_rd --target ${target}
-  python run_qm9.py --h 3 --model k13_GNN_sub --save_appendix _k13_h3_spd_rd --use_rd --target ${target}
-  python run_qm9.py --h 3 --model k123_GNN_sub --save_appendix _k123_h3_spd_rd --use_rd --target ${target}
+  #python run_qm9.py --h 3 --model k1_GNN_sub --save_appendix _k1_h3_spd_rd --use_rd --target ${target}
+  #python run_qm9.py --h 3 --model k12_GNN_sub --save_appendix _k12_h3_spd_rd --use_rd --target ${target}
+  #python run_qm9.py --h 3 --model k13_GNN_sub --save_appendix _k13_h3_spd_rd --use_rd --target ${target}
+  #python run_qm9.py --h 3 --model k123_GNN_sub --save_appendix _k123_h3_spd_rd --use_rd --target ${target}
 
-  python run_qm9.py --h 3 --model k1_GNN_sub --save_appendix _k1_h3_no --node_label no --target ${target}
-  python run_qm9.py --h 3 --model k12_GNN_sub --save_appendix _k12_h3_no --node_label no --target ${target}
-  python run_qm9.py --h 3 --model k13_GNN_sub --save_appendix _k13_h3_no --node_label no --target ${target}
-  python run_qm9.py --h 3 --model k123_GNN_sub --save_appendix _k123_h3_no --node_label no --target ${target}
-  '''
+  #python run_qm9.py --h 3 --model k1_GNN_sub --save_appendix _k1_h3_no --node_label no --target ${target}
+  #python run_qm9.py --h 3 --model k12_GNN_sub --save_appendix _k12_h3_no --node_label no --target ${target}
+  #python run_qm9.py --h 3 --model k13_GNN_sub --save_appendix _k13_h3_no --node_label no --target ${target}
+  #python run_qm9.py --h 3 --model k123_GNN_sub --save_appendix _k123_h3_no --node_label no --target ${target}
   
-  python run_qm9.py --model k1_GNN --save_appendix _k1_RNI --RNI --target ${target}
+  #python run_qm9.py --model k1_GNN --save_appendix _k1_RNI --RNI --target ${target}
   #python run_qm9.py --h 3 --model k1_GNN_sub --save_appendix _k1_h3_spd_rd_RNI --RNI --use_rd --target ${target}
+  python run_qm9.py --h 1 --model NestedPPGN --save_appendix _nppgn_h1_l1_spd_rd --layers 1 --node_label spd --use_rd --target ${target} --batch_size 16
 done
